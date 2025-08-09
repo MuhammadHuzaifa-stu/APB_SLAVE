85e82a5bc0d0e0ffbda3542c4b949c79924abd06
MuhammadHuzaifa-stu <2020ee118@stduent.uet.edu.pk>
2025-08-09 15:08:43 +0500

    added makefile, fist

---
+++ UVM/flist.f
@@ -0,0 +1,17 @@
+# RTL files
+rtl/apb_mem.sv
+
+# TB files
+tb/interface.sv
+tb/top_tb.sv
+
+# UVM environment files
+uvm_env/apb_seq_item.sv
+uvm_env/apb_sequence.sv
+uvm_env/apb_sequencer.sv
+uvm_env/apb_driver.sv
+uvm_env/apb_monitor.sv
+uvm_env/apb_agent.sv
+uvm_env/apb_scoreboard.sv
+uvm_env/apb_env.sv
+uvm_env/apb_test.sv
